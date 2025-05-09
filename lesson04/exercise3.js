f9eed1c9063df0cd68e508a62119f39a21465d28
Tasper <tiffanyasper@gmail.com>
2025-05-09 14:55:21 -0700

    added JS

---
+++ lesson04/exercise3.js
@@ -0,0 +1,11 @@
+let myPets = ["cats", "dogs", "chickens", "iguanas", "tarantulas"];
+
+myPets.forEach((element, index) => {
+  console.log(index + " " + element);
+});
+
+("0 cats");
+("1 dogs");
+("2 chickens");
+("3 iguanas");
+("4 tarantulas");
