a0d684b647a4b7556449527b94e76a4196b7fc21
Tasper <tiffanyasper@gmail.com>
2025-05-10 17:08:00 -0700

    added JS

---
+++ lesson04/exercise4.5.js
@@ -0,0 +1,85 @@
+const cardArr = [
+  ["hearts", "diamonds", "clubs", "spades"],
+  [
+    "ace",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "jack",
+    "queen",
+    "king",
+  ],
+];
+
+let suits = cardArr[0];
+let cards = cardArr[1];
+
+for (let i = 0; i < suits.length; i++) {
+  for (let j = 0; j < cards.length; j++) {
+    console.log(cards[j] + " of " + suits[i]);
+  }
+}
+
+("ace of hearts");
+("one of hearts");
+("two of hearts");
+("three of hearts");
+("four of hearts");
+("five of hearts");
+("six of hearts");
+("seven of hearts");
+("eight of hearts");
+("nine of hearts");
+("ten of hearts");
+("jack of hearts");
+("queen of hearts");
+("king of hearts");
+("ace of diamonds");
+("one of diamonds");
+("two of diamonds");
+("three of diamonds");
+("four of diamonds");
+("five of diamonds");
+("six of diamonds");
+("seven of diamonds");
+("eight of diamonds");
+("nine of diamonds");
+("ten of diamonds");
+("jack of diamonds");
+("queen of diamonds");
+("king of diamonds");
+("ace of clubs");
+("one of clubs");
+("two of clubs");
+("three of clubs");
+("four of clubs");
+("five of clubs");
+("six of clubs");
+("seven of clubs");
+("eight of clubs");
+("nine of clubs");
+("ten of clubs");
+("jack of clubs");
+("queen of clubs");
+("king of clubs");
+("ace of spades");
+("one of spades");
+("two of spades");
+("three of spades");
+("four of spades");
+("five of spades");
+("six of spades");
+("seven of spades");
+("eight of spades");
+("nine of spades");
+("ten of spades");
+("jack of spades");
+("queen of spades");
+("king of spades");
