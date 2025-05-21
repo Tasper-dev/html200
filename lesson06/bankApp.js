af6898fa24eeb41ed76f8833e2c826ba7608e4e4
Tasper <tiffanyasper@gmail.com>
2025-05-20 17:42:24 -0700

    created file and added content

---
+++ lesson06/bankApp.js
@@ -0,0 +1,25 @@
+function menuOptions() {
+    let input = prompt(
+        'Enter Q to quit program,
+        Enter W to withdraw funds,
+        Enter D to deposit funds,
+        Enter B to view account balance', 'Q'
+      ); 
+  }
+  switch (input) {
+    case "Q":
+        alert("Quitting program.")
+      break;
+    case "W":
+      prompt("Enter amount to withdraw:");
+      break;
+    case "D":
+      prompt("Enter amount to deposit:");
+      break;
+    case "B":
+      prompt("Account Balance: $0.00");
+      break;
+    default:
+      alert("Not a valid menu option. Enter a valid menu option.");
+  }
+}
