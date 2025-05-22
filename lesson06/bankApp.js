8dd80b119630806c796083b7d27e762dd0579f12
Tasper <tiffanyasper@gmail.com>
2025-05-21 17:43:51 -0700

    added to js

--- lesson06/bankApp.js
+++ lesson06/bankApp.js
@@ -1,25 +1,30 @@
+window.onload = function () {
+  menuOptions();
+};
 function menuOptions() {
-    let input = prompt(
-        'Enter Q to quit program,
-        Enter W to withdraw funds,
-        Enter D to deposit funds,
-        Enter B to view account balance', 'Q'
-      ); 
-  }
-  switch (input) {
-    case "Q":
-        alert("Quitting program.")
-      break;
-    case "W":
-      prompt("Enter amount to withdraw:");
-      break;
-    case "D":
-      prompt("Enter amount to deposit:");
-      break;
-    case "B":
-      prompt("Account Balance: $0.00");
-      break;
-    default:
-      alert("Not a valid menu option. Enter a valid menu option.");
-  }
+  let input;
+  do {
+    input = prompt(
+      `Enter Q to quit program.
+        Enter W to withdraw funds.
+        Enter D to deposit funds.
+        Enter B to view account balance.`
+    );
+    switch (input?.toUpperCase()) {
+      case "Q":
+        alert("Quitting program.");
+        break;
+      case "W":
+        prompt("Enter amount to withdraw:");
+        break;
+      case "D":
+        prompt("Enter amount to deposit:");
+        break;
+      case "B":
+        alert("Account Balance: $");
+        break;
+      default:
+        alert("Not a valid menu option. Enter a valid menu option.");
+    }
+  } while (input?.toUpperCase() !== "Q");
 }
