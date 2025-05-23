66614add9e48758615b00931ea6a8a5906463a63
Tasper <tiffanyasper@gmail.com>
2025-05-22 21:07:05 -0700

    added js

--- lesson06/bankApp.js
+++ lesson06/bankApp.js
@@ -3,6 +3,7 @@ window.onload = function () {
 };
 function menuOptions() {
   let input;
+  let balance = 0;
   do {
     input = prompt(
       `Enter Q to quit program.
@@ -15,13 +16,29 @@ function menuOptions() {
         alert("Quitting program.");
         break;
       case "W":
-        prompt("Enter amount to withdraw:");
+        let withdrawAmount = parseFloat(prompt("Enter amount to withdraw:"));
+        if (isNaN(withdrawAmount) || withdrawAmount <= 0) {
+          alert("Invalid withdrawal amount.");
+        } else if (withdrawAmount > balance) {
+          alert("Insufficient funds.");
+        } else {
+          balance -= withdrawAmount;
+          alert(`New Account Balance: $${balance.toFixed(2)}`);
+        }
         break;
       case "D":
-        prompt("Enter amount to deposit:");
+        let depositAmount = parseFloat(prompt("Enter amount to deposit:"));
+        if (isNaN(depositAmount) || depositAmount <= 0) {
+          alert("Invalid deposit amount.");
+        } else if (depositAmount > 50000) {
+          alert("Cannot deposit more than $50,000");
+        } else {
+          balance += depositAmount;
+          alert(`New Account Balance: $${balance.toFixed(2)}`);
+        }
         break;
       case "B":
-        alert("Account Balance: $");
+        alert(`Account Balance: $${balance.toFixed(2)}`);
         break;
       default:
         alert("Not a valid menu option. Enter a valid menu option.");
