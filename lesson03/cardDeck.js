4ec687b28d550a71d9174f6c5daf6477013556b3
Tasper <tiffanyasper@gmail.com>
2025-06-22 16:37:09 -0700

    js for loop

---
+++ lesson03/cardDeck.js
@@ -0,0 +1,20 @@
+const cardDeck = [
+  "Ace",
+  "One",
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten",
+  "Jack",
+  "Queen",
+  "King",
+];
+
+for (let i = 0; i < cardDeck.length; i++) {
+  console.log(cardDeck[i]);
+}
